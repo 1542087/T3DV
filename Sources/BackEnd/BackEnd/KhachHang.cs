4d08d8b2ec41843baab0ef9793abca9d7d16ee3d
vinh.tv <vinh.tv2003@gmail.com>
2017-06-03 22:17:22 +0700

    [C# backend]remove unuse package

--- Sources/BackEnd/BackEnd/KhachHang.cs
+++ Sources/BackEnd/BackEnd/KhachHang.cs
@@ -1,6 +1,5 @@
 ﻿namespace CreditManagement.Models
 {
-    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
